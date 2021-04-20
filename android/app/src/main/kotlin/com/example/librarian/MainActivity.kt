0ba9298829399dbed35666f82fa1ee9d36541e10
Patrick Sumarli <tricksumarli@gmail.com>
2021-04-20 14:48:20 +0700

    Initialised Flutter app

---
+++ android/app/src/main/kotlin/com/example/librarian/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.librarian
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const Color darkBlue = Color.fromRGBO(10, 4, 60, 1);
+const Color blue = Color.fromRGBO(3, 80, 111, 1);
+const Color lightGreen = Color.fromRGBO(163, 221, 203, 1);
+const Color lightPink = Color.fromRGBO(255, 227, 222, 1);
